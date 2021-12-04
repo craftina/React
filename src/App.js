1c6c62bf0ccdd926dd52e64de1f70683695c75ad
craftina <hristina.rum.hristova@gmail.com>
2021-12-04 19:14:28 +0200

    Extract components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import Header from "./components/Header.js";
+import Welcome from "./components/Welcome.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Create from "./components/Create.js";
+import Edit from "./components/Edit.js";
+import Details from "./components/Details.js";
+import Catalog from "./components/Catalog.js";
+
 
 function App() {
   return (
     <div>
       
+    <div id="box">
+
+        <Header />
+        <Welcome />
+        <Login />
+        <Register />
+        <Create />
+        <Edit />
+        <Details />
+        <Catalog />
+
+        <main id="main-content">
+        </main>
+
+        
+    </div>
+
     </div>
   );
 }
